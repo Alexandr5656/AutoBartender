546914918c2a40831b7e05429e9f322b85c10300
Alexandr5656 <alexburbano5656@gmail.com>
2022-01-03 12:06:27 -0500

    Installed scale code

--- Bartender.py
+++ Bartender.py
@@ -2,17 +2,17 @@ import json
 import Drink as drinks
 import Pump as pumps
 import time
-#from scale.hx711 import HX711
+from scale.hx711 import HX711
 class Bartender:
     pumpLocation = dict()
     drinkList = dict()
-    #scale = HX711(16,12)
+    scale = HX711(16,12)
     recipeList = dict()
     def __init__(self):
         self.getPumps()
         self.getDrinks()
-        #self.scale.set_reading_format("MSB", "MSB")
-        #self.scale.set_reference_unit(453)
+        self.scale.set_reading_format("MSB", "MSB")
+        self.scale.set_reference_unit(453)
 
     def getPumps(self):
         #Set a place where I can do invalidate a drink if a pump is not there
@@ -21,7 +21,7 @@ class Bartender:
         gpioPins = json.load(open('./src/pins.json'))
         print(gpioPins)
         for pump in pumpJson: 
-            #self.pumpLocation[pumpJson[pump]] = pumps.Pump(int(pump))
+            self.pumpLocation[pumpJson[pump]] = pumps.Pump(int(pump))
             self.pumpLocation[pumpJson[pump]] = pumps.Pump(int(gpioPins[pump]))
             self.pumpLocation[pumpJson[pump]].stopPump()
 
@@ -37,12 +37,12 @@ class Bartender:
 
     def makeDrink(self,drink):
         for ingredient in self.drinkList[drink].getRecipe():
-            #self.scale.reset()
-            #self.scale.tare()
-            #while self.scale.get_weight(5) < self.drinkList[drink].getAmount(ingredient):
-                #self.pumpLocation[ingredient].runPump()
-                #self.scale.power_down()
-                #self.scale.power_up()
-                #time.sleep(0.1)
+            self.scale.reset()
+            self.scale.tare()
+            while self.scale.get_weight(5) < self.drinkList[drink].getAmount(ingredient):
+                self.pumpLocation[ingredient].runPump()
+                self.scale.power_down()
+                self.scale.power_up()
+                time.sleep(0.1)
             self.pumpLocation[ingredient].stopPump()
 
