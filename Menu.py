f04272b51e6122860cd7f9a7af9db3f09eada1d5
Alexandr5656 <alexburbano5656@gmail.com>
2022-01-03 12:03:49 -0500

    Fixed Scale Code

--- Menu.py
+++ Menu.py
@@ -1,4 +1,6 @@
 import sys
+from functools import partial
+
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QWidget, QScrollArea, QHBoxLayout
@@ -11,63 +13,62 @@ from Drink import *
 
 class MainMenu(object):
     def setupUI(self, MainWindow):
-        MainWindow.setGeometry(50, 50, 400, 450)
-        MainWindow.setFixedSize(400, 450)
         MainWindow.setWindowTitle("UIWindow")
         self.centralwidget = QWidget(MainWindow)
-        # mainwindow.setWindowIcon(QtGui.QIcon('PhotoIcon.png'))
-        self.CPSBTN = QPushButton('text', self.centralwidget)
-        self.CPSBTN.move(50, 350)
+        self.CPSBTN = QPushButton('Drink Menu', self.centralwidget)
+        self.CPSBTN.move(10, 10)
         MainWindow.setCentralWidget(self.centralwidget)
 
-
+#
 class DrinkMenu(object):
-    def setupUI(self, MainWindow):
-        MainWindow.setGeometry(50, 50, 400, 450)
-        MainWindow.setFixedSize(400, 450)
+    def setupUI(self, MainWindow, app):
+        MainWindow.setGeometry(0, 0, app.primaryScreen().size().width(), app.primaryScreen().size().height())
+        MainWindow.setFixedSize(app.primaryScreen().size().width(), app.primaryScreen().size().height())
         MainWindow.setWindowTitle("UIToolTab")
         self.centralwidget = QWidget(MainWindow)
-        self.ToolsBTN = QPushButton("text2", self.centralwidget)
-        self.ToolsBTN.move(100, 350)
-        #self.createButtons()
-        #self.createButtons(MainMenu)
-        self.alex()
+        self.ToolsBTN = QPushButton("Back", self.centralwidget)
+        self.ToolsBTN.move(10, 10)
+        self.button_width = int(app.primaryScreen().size().width() / 3)
+        self.button_height = int(app.primaryScreen().size().height() / 3)
+        self.createButtons()
         MainWindow.setCentralWidget(self.centralwidget)
-    def alex(self):
-        pass
+
+    def alex(self,drinke):
+        print("{}%".format(drinke))
+
     def createButtons(self):
         self.bartender = Bartender()
         drinks = self.bartender.getList()
-        self.scroll = QScrollArea()  # Scroll Area which contains the widgets, set as the centralWidget
-        self.widget = QWidget()  # Widget that contains the collection of Vertical Box
-        self.hbox = QHBoxLayout()  # The Vertical Box that contains the Horizontal Boxes of  labels and buttons
-
-        for x in range(len(drinks)):
-            pybutton = QPushButton()
-            pybutton.setText(drinks[x].getName())
-            pybutton.clicked.connect(lambda: self.bartender.makeDrink(drinks[x]))
-            # TODO: Work on button positioning and multiple pages?
-            pybutton.setFixedWidth(self.button_width)
-            pybutton.setFixedHeight(self.button_height)
-            pybutton.setLayoutDirection(QtCore.Qt.RightToLeft)
-            pybutton.setIcon(QtGui.QIcon('media/drinks/empty.png'))
-            pybutton.setIconSize(QSize(int(self.button_width/3),int(self.button_height/3)))
-            self.hbox.addWidget(pybutton)
+        self.scroll = QScrollArea(self.centralwidget)
+        self.widget = QWidget()
+        self.hbox = QHBoxLayout()
+        self.buttons = []
+        for drink in drinks:
+            self.buttons.append(QPushButton())
+            self.buttons[-1].setText(drink)
+            self.buttons[-1].setFixedWidth(self.button_width)
+            self.buttons[-1].setFixedHeight(self.button_height*2)
+            self.buttons[-1].clicked.connect(partial(self.bartender.makeDrink, drink=drink))
+            self.buttons[-1].setIcon(QtGui.QIcon('media/drinks/empty.png'))
+            self.buttons[-1].setIconSize(QSize(int(self.button_width/3),int(self.button_height/3)))
+            self.hbox.addWidget(self.buttons[-1])
 
         self.widget.setLayout(self.hbox)
 
-        # Scroll Area Properties
         self.scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
         self.scroll.setWidgetResizable(True)
         self.scroll.setWidget(self.widget)
-
-        #mainWindow.setCentralWidget(self.scroll)
-
+        self.scroll.setFixedWidth(self.button_width*3)
+        self.scroll.setFixedHeight(self.button_height*2)
+        self.scroll.move(0,int(self.button_height/2))
 
 class MainWindow(QMainWindow):
-    def __init__(self, bartender, parent=None,):
+    def __init__(self, bartender,app, parent=None,):
         super(MainWindow, self).__init__(parent)
+        self.setFixedWidth(app.primaryScreen().size().width())
+        self.setFixedHeight(app.primaryScreen().size().height())
+        self.app = app
         self.uiMainMenu = MainMenu()
         self.uiDrinkMenu = DrinkMenu()
         self.startMainMenu()
@@ -78,7 +79,7 @@ class MainWindow(QMainWindow):
         self.show()
 
     def startDrinkMenu(self):
-        self.uiDrinkMenu.setupUI(self)
+        self.uiDrinkMenu.setupUI(self,self.app)
         self.uiDrinkMenu.ToolsBTN.clicked.connect(self.startMainMenu)
         self.uiDrinkMenu.createButtons()
         self.show()
--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication
 
 import Bartender as bar
-#import Menu as menu
+import Menu as menu
 
 import sys
 #from PyQt5 import QtWidgets
 a = bar.Bartender()
 if __name__ == "__main__":
-    #app = QApplication(sys.argv)
-    #w = menu.MainWindow(a)
-    #sys.exit(app.exec_())
-    while 1==1:
-        name = input('Choose a drink \n')
-        a.makeDrink(name)
+    app = QApplication(sys.argv)
+    w = menu.MainWindow(a,app)
+    sys.exit(app.exec_())
+    #while 1==1:
+    #    name = input('Choose a drink \n')
+    #    a.makeDrink(name)
